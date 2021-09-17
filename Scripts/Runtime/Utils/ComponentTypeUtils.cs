771a27a6d960769d858cf8555015cfb9ec550c41
Alexander Travkin <alexander.travkin@varwin.com>
2021-09-17 16:33:18 +0300

    Helper class for serialize ComponentType by ID (for network)

---
+++ Scripts/Runtime/Utils/ComponentTypeUtils.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AffenECS
+{
+    public static class ComponentTypeUtils
+    {
+        private static readonly Dictionary<Type, ushort> SerializedTypesByType = new Dictionary<Type, ushort>();
+        private static readonly Dictionary<ushort, Type> SerializedTypesByIndex = new Dictionary<ushort, Type>();
+        
+        static ComponentTypeUtils()
+        {
+            var ecsComponentTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.GetInterfaces().Contains(typeof(EcsComponent)))
+                .OrderBy(x => x.FullName);
+
+            ushort index = 0;
+            foreach (Type ecsComponentType in ecsComponentTypes)
+            {
+                SerializedTypesByType.Add(ecsComponentType, index);
+                SerializedTypesByIndex.Add(index, ecsComponentType);
+                index++;
+            }
+        }
+
+        public static ushort SerializeType(this EcsComponent ecsComponent)
+        {
+            return SerializedTypesByType[ecsComponent.GetType()];
+        }
+
+        public static Type DeserializeType(this ushort ecsComponentSerializedTypeIndex)
+        {
+            return SerializedTypesByIndex[ecsComponentSerializedTypeIndex];
+        }
+    }
+}
